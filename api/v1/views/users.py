e7726a55878b8716f2e7fdc79b782f142d4e4de2
DINAMOW <meemoo102039@gmail.com>
2024-03-10 17:11:53 +0200

    refactor: add doc and fix the pycodestyle

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,14 +1,37 @@
 #!/usr/bin/python3
-""" Index """
+"""This module contains the views for user operations.
+
+The module defines Flask routes for user-related operations such as
+retrieving user information, creating new users, updating user information,
+and handling user login and logout.
+
+Routes:
+    - /user: Endpoint for user operations.
+    - /login: Endpoint for user login and logout.
+"""
 from api.v1.views import app_views
-from flask import jsonify, request, session, redirect
-from flask_mail import Message
+from flask import jsonify, request, session
 from models import AUTH
 
 
 @app_views.route('/user', methods=['GET', 'POST', 'PUT'], strict_slashes=False)
 def users():
-    """ Status of API """
+    """
+    Endpoint for user operations.
+
+    GET: Retrieves the user information based on the session ID.
+    POST: Creates a new user based on the form data.
+    PUT: Updates the user information based on the form data.
+
+    Returns:
+        - If successful:
+            - GET: JSON representation of\
+                the user information with status code 200.
+            - POST and PUT: Empty JSON response with status code 200.
+        - If unsuccessful:
+            - JSON response with an error message and status code 400.
+        - POST: JSON response with an error message and status code 201.
+    """
     if request.method == 'GET':
         try:
             user = AUTH.get_user(session['session_id'])
@@ -30,7 +53,20 @@ def users():
 
 @app_views.route('/login', methods=['POST', 'DELETE'], strict_slashes=False)
 def login():
-    """ login endpoint """
+    """Handles the login endpoint.
+
+    This function handles the login endpoint for the API.
+    It allows users to log in by providing their email and password.
+    The function supports both POST and DELETE methods.
+
+    Returns:
+        A JSON response containing the\
+            email and a message indicating whether the user
+        has successfully logged in or logged out.
+
+    Raises:
+        ValueError: If there is an error during the login or logout process.
+    """
     if request.method == 'POST':
         try:
             session['session_id'] = AUTH.login(request.form.get('email'),
@@ -48,23 +84,3 @@ def login():
         return jsonify({"error": str(e)}), 400
     return jsonify({"email": request.form.get('email'),
                     "message": "logged out"})
-
-# @app_views.route('/reset_password', methods=['POST'], strict_slashes=False)
-# def reset_password():
-#     """ reset password """
-#     msg = Message('Subject: Test mail',
-#                   sender=Config.MAIL_USERNAME,
-#                   recipients=['meemoo102039@gmail.com'])
-#     msg.body = "This is the email body"
-#     try:
-#         mail.send(msg)
-#         return 'done'
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 400
-    # try:
-    #     AUTH.reset_password(request.form.get('email'))
-    #     return jsonify({"email": request.form.get('email'),
-    #                     "message": "password reset"}), 200
-    # except ValueError as e:
-    #     return jsonify({"error": str(e)}), 400
-    # return jsonify({}), 200
