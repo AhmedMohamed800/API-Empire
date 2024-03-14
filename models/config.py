9dbd28f0236ea10ad01bfd407035fca9837e475f
DINAMOW <meemoo102039@gmail.com>
2024-03-14 14:32:59 +0200

    perf: add default sender instead of setting the sender each time

--- models/config.py
+++ models/config.py
@@ -3,8 +3,9 @@
 class Config(object):
     """ config class """
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = 'api.empire0@gmail.com'
     MAIL_PASSWORD = 'juoe smrg pmmm rkbs'
+    MAIL_DEFAULT_SENDER = 'api.empire0@gmail.com'
