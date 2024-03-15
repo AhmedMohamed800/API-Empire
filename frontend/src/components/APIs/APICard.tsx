a7c36f7fd7f91aeb125459ee12c12da9b176563e
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-03-15 15:21:10 +0200

    feat: add search bar and filter to APIs page

--- frontend/src/components/APIs/APICard.tsx
+++ frontend/src/components/APIs/APICard.tsx
@@ -25,25 +25,31 @@ const APICard: React.FC<APICardProps> = ({
   return (
     <div
       onClick={handleConnect}
-      className="api-card relative flex cursor-pointer flex-col gap-2 overflow-hidden rounded-md border-2 border-neutral-400 p-3 "
+      className="api-card group relative flex cursor-pointer flex-col  gap-2 overflow-hidden rounded-md border-2 border-neutral-400 p-3 hover:border-primary "
     >
       <div
         style={{
-          backgroundImage: `linear-gradient(180deg, rgba(99, 91, 255, 0) 0%, rgba(99, 91, 255, 0.5) 100%), url(${image_cover})`,
+          backgroundImage: `linear-gradient(180deg, rgba(136, 136, 136, 0.00) 0%, rgba(136, 136, 136, 0.50) 93.5%), url(${image_cover})`,
         }}
-        className="h-40 w-full rounded-md border-2 border-neutral-400 bg-cover bg-center bg-no-repeat "
+        className="h-40 w-full rounded-md border-2 border-neutral-400 bg-cover bg-center bg-no-repeat group-hover:border-primary "
+        onMouseOver={(e) =>
+          (e.currentTarget.style.backgroundImage = `"linear-gradient(180deg, rgba(99, 91, 255, 0.00) 0%, rgba(99, 91, 255, 0.50) 100%), url(${image_cover})`)
+        }
+        onMouseOut={(e) =>
+          (e.currentTarget.style.backgroundImage = `linear-gradient(180deg, rgba(136, 136, 136, 0.00) 0%, rgba(136, 136, 136, 0.50) 93.5%), url(${image_cover})`)
+        }
       ></div>
       <h4 className=" border-b-2 border-white pb-2 text-center  font-semibold">
         {title}
       </h4>
       <p className=" text-sm text-neutral-300">{description}</p>
       <div className="flex items-center justify-between gap-2">
-        <button className="  rounded-md  bg-primary px-3 py-2 text-white">
+        <button className="  rounded-md  bg-neutral-400 px-3 py-2 text-white group-hover:bg-primary">
           Connect
         </button>
         <p className="text-sm">Free 7-day trial</p>
       </div>
-      <p className=" absolute -right-4 top-2 rotate-45 bg-neutral-400 px-3 text-[10px]">
+      <p className=" absolute -right-4 top-2 rotate-45 bg-neutral-400 px-3 text-[10px] group-hover:bg-primary">
         {category}
       </p>
     </div>
--- frontend/src/pages/APIs.tsx
+++ frontend/src/pages/APIs.tsx
@@ -1,94 +1,123 @@
 import React from "react";
-import { useEffect, useState, useMemo, useRef } from "react";
-import APICard from "../components/APIs/APICard.tsx";
-import axios from "axios";
 const arrow = require("../assets/apis/arrow.svg") as string;
-
-interface APICardProps {
-  title: string;
-  description: string;
-  image_cover: string;
-  id: string;
-  category: string;
-}
+import { usePaginatedAPIs } from "../components/APIs/APIsHook.tsx";
+import { useState } from "react";
+const search = require("../assets/apis/search.svg") as string;
+const filter = require("../assets/apis/filter.svg") as string;
 
 export const APIs = () => {
-  const [apis, setApis] = useState<APICardProps[]>([]);
-  const [page, setPage] = useState<number>(1);
-  const topRef = useRef(null);
   const apisPerPage = 20;
-
-  const paginate = (pageNumber) => {
-    setPage(pageNumber);
-    const url = new URL(window.location.href);
-    url.searchParams.set("page", pageNumber);
-    window.history.replaceState({}, "", url);
-  };
-
-  useEffect(() => {
-    paginate(1);
-    document.title = "APIs";
-    const APIsURL = `${process.env.REACT_APP_API_URL}/api/v1/service/all`;
-    if (sessionStorage.getItem("apis")) {
-      const parsedApis = JSON.parse(sessionStorage.getItem("apis") || "");
-      if (parsedApis) {
-        setApis(parsedApis);
-        return;
-      }
-    }
-    axios
-      .get(APIsURL)
-      .then((response) => {
-        sessionStorage.setItem("apis", JSON.stringify(response.data.message));
-        setApis(response.data.message);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  const computedApis = useMemo(() => {
-    if (apis && Array.isArray(apis) && apis.length > 0) {
-      const startIndex = (page - 1) * apisPerPage;
-      const endIndex = startIndex + apisPerPage;
-      const apisForPage = apis.slice(startIndex, endIndex);
-
-      return apisForPage.map((api, _) => {
-        return (
-          <APICard
-            key={api.id}
-            title={api.title}
-            description={api.description}
-            image_cover={api.image_cover}
-            url={api.id}
-            category={api.category}
-          />
-        );
-      });
-    }
-    return null;
-  }, [apis, page]);
-
-  function generateUUID() {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-      /[xy]/g,
-      function (c) {
-        var r = (Math.random() * 16) | 0,
-          v = c === "x" ? r : (r & 0x3) | 0x8;
-        return v.toString(16);
-      },
-    );
-  }
+  const [filterModel, setfilterModel] = useState<boolean>(true);
+  const {
+    computedApis,
+    paginate,
+    topRef,
+    page,
+    filteredApis,
+    generateUUID,
+    setSearchTerm,
+    setSelectedCategory,
+    selectedCategory,
+    searchTerm,
+  } = usePaginatedAPIs(apisPerPage);
 
   return (
-    <div className="z-10 mx-20 my-10 grid gap-6 max-sm:mx-5 ">
+    <div className="z-10 flex flex-col  max-sm:mx-5 ">
+      <header
+        className={`flex h-16 items-center justify-between gap-2 border-b-2 border-white px-20 ${filterModel && "mb-10"}`}
+      >
+        <div className="flex grow gap-2">
+          <img src={search} alt="search" width="24px" height="24px" />
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search for your API..."
+            className="h-10 grow bg-transparent focus:border-none focus:outline-none"
+          />
+        </div>
+        <img
+          src={filter}
+          alt="filter"
+          width="24px"
+          height="24px"
+          className="cursor-pointer"
+          onClick={() => setfilterModel(!filterModel)}
+        />
+      </header>
+      <ul
+        className={`mb-6 grid h-24 grid-flow-col place-content-center gap-2 bg-primary ${filterModel && "hidden"}`}
+      >
+        <li
+          data-category="weather"
+          className={
+            selectedCategory === "weather" ? "search_active" : "search_inactive"
+          }
+          onClick={(e) => {
+            const target = e.target as HTMLElement;
+            setSelectedCategory(target.dataset.category as string);
+            paginate(1);
+          }}
+        >
+          Weather
+        </li>
+        <li
+          data-category="data"
+          className={
+            selectedCategory === "data" ? "search_active" : "search_inactive"
+          }
+          onClick={(e) => {
+            const target = e.target as HTMLElement;
+            setSelectedCategory(target.dataset.category as string);
+            paginate(1);
+          }}
+        >
+          Data
+        </li>
+        <li
+          data-category="finance"
+          className={
+            selectedCategory === "finance" ? "search_active" : "search_inactive"
+          }
+          onClick={(e) => {
+            const target = e.target as HTMLElement;
+            setSelectedCategory(target.dataset.category as string);
+
+            paginate(1);
+          }}
+        >
+          Finance
+        </li>
+        <li
+          data-category="ip"
+          className={
+            selectedCategory === "ip" ? "search_active" : "search_inactive"
+          }
+          onClick={(e) => {
+            const target = e.target as HTMLElement;
+            setSelectedCategory(target.dataset.category as string);
+            setfilterModel(false);
+            paginate(1);
+          }}
+        >
+          IP
+        </li>
+      </ul>
       <main
         ref={topRef}
-        className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+        className={`mx-20 mb-6 grid grow  grid-cols-1 grid-rows-1 items-center gap-5  ${computedApis.length > 0 && "md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"}`}
       >
-        {computedApis}
+        {computedApis.length > 0 ? (
+          computedApis
+        ) : (
+          <h1 className="order-3 text-center text-3xl ">
+            THERE IS NO API HERE!
+          </h1>
+        )}
       </main>
-      <section className="flex flex-row-reverse items-center justify-between">
+      <section
+        className={`mx-20 mb-10 flex flex-row-reverse items-center justify-between ${computedApis.length > 0 || "hidden"}`}
+      >
         <div className="flex gap-3">
           <button
             className="rounded-full border border-white p-2"
@@ -112,7 +141,7 @@ export const APIs = () => {
           <button
             className="rounded-full border border-white p-2"
             onClick={() => {
-              if (page < Math.ceil(apis.length / 20)) {
+              if (page < Math.ceil(filteredApis / 20)) {
                 paginate(page + 1);
                 (topRef.current as HTMLElement | null)?.scrollIntoView({
                   behavior: "smooth",
@@ -123,8 +152,8 @@ export const APIs = () => {
             <img src={arrow} alt="arrow-right" width="24px" height="24px" />
           </button>
         </div>
-        <div className="flex gap-2">
-          {[...Array(Math.ceil(apis.length / 20)).keys()].map((_, index) => {
+        <div className=" flex gap-2">
+          {[...Array(Math.ceil(filteredApis / 20)).keys()].map((_, index) => {
             const uuid = generateUUID();
             if (index === page - 1) {
               return (
