5050693e72f2abbfbf50dbdd2d46faa5b37f0108
DINAMOW <meemoo102039@gmail.com>
2024-03-15 15:38:39 +0200

    add-endpoint: for the endpoints (to be cont)

--- api/v1/views/service.py
+++ api/v1/views/service.py
@@ -8,5 +8,19 @@ import base64
 @app_service.route('/all', methods=['GET'], strict_slashes=False)
 def get_weather():
     """ Get weather """
-    apis = SERVICE.get_all()
-    return jsonify({"message": apis}), 200
+    return jsonify(SERVICE.get_all()), 200
+
+
+@app_service.route('/<service_id>', methods=['GET'], strict_slashes=False)
+def get_service(service_id):
+    """ Get service """
+    try:
+        return jsonify(SERVICE.get(service_id)), 200
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 404
+
+
+@app_service.route('/endpoints', methods=['GET'], strict_slashes=False)
+def get_endpoints():
+    """ Get all endpoints """
+    return jsonify(SERVICE.get_all_endpoints()), 200
