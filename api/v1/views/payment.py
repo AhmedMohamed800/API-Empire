64693ad88dde93cce43fc81f129d8bafe9ecf3e2
DINAMOW <meemoo102039@gmail.com>
2024-03-16 22:21:28 +0200

    add-endpoint: api/v1/payment/create(POST) -> create payment add-endpoint: api/v1/payment/execute(POST) -> execute the payment

--- api/v1/views/payment.py
+++ api/v1/views/payment.py
@@ -12,28 +12,29 @@ import paypalrestsdk
 
 
 paypalrestsdk.configure({
-  "mode": "sandbox",  # Change this to "live" for production
+  "mode": "sandbox",
   "client_id": "Acc2NHlsRMBVgXGafvkkkvd1QFCRV4HOAPRqfysiLrGYRu_lyKTHDcl4aGbe3zyDe7YG9yd9K2jsv0qe",
   "client_secret": "EPX26zVDzTM2-TSaZu2HthOZnFLIGQJJlQpKEus3D1lbW8pT_aH-UABJkk1YyCB0JzkM3CFk8bVUU67i"
 })
 
 
-app_payment.route('/create', methods=['POST'], strict_slashes=False)
+@app_payment.route('/create', methods=['POST'], strict_slashes=False)
 def create_payment():
     """Create a payment """
-    data = request.get_json()
+    data = request.form.to_dict()
+    print(data)
     payment = paypalrestsdk.Payment({
         "intent": "sale",
         "payer": {
             "payment_method": "paypal"},
         "redirect_urls": {
-            "return_url": "http://localhost:5000/payment/execute",
-            "cancel_url": "http://localhost:5000/"},
+            "return_url": "http://localhost:5000/api/v1/payment/execute",
+            "cancel_url": "http://localhost:5000/api/v1/payment/cancel"},
         "transactions": [{
             "item_list": {
                 "items": [{
-                    "name": "item",
-                    "sku": "item",
+                    "name": f"{data['name']}",
+                    "sku": f"{data['sku']}",
                     "price": f"{data['price']}",
                     "currency": f"{data['currency']}",
                     "quantity": 1}]},
@@ -42,17 +43,16 @@ def create_payment():
                 "currency": f"{data['currency']}"},
             "description": "This is the payment transaction description."}]})
     if payment.create():
-        return jsonify({"id": payment.id}), 201
+        return jsonify({"paymentID": payment.id}), 201
     else:
         return jsonify({"error": payment.error}), 400
 
 
-app_payment.route('/execute', methods=['POST'], strict_slashes=False)
+@app_payment.route('/execute', methods=['POST'], strict_slashes=False)
 def execute_payment():
     """Execute a payment """
-    data = request.get_json()
-    payment = paypalrestsdk.Payment.find(data['paymentID'])
-    if payment.execute({"payer_id": data['payerID']}):
-        return jsonify({"id": payment.id}), 200
+    payment = paypalrestsdk.Payment.find(request.form['paymentID'])
+    if payment.execute({"payer_id": request.form['payerID']}):
+        return jsonify({"result": payment.id}), 200
     else:
         return jsonify({"error": payment.error}), 400
