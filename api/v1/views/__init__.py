633b31781c4dec71103b0db23f0e407a67a0dabc
DINAMOW <meemoo102039@gmail.com>
2024-03-16 12:12:05 +0200

    fix: scripy.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,8 +5,10 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 app_service = Blueprint('app_service', __name__, url_prefix='/api/v1/service')
 app_apis = Blueprint('app_apis', __name__, url_prefix='/api')
+app_payment = Blueprint('app_payment', __name__, url_prefix='/api/v1/payment')
 
 from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.service import *
 from api.v1.views.apis import *
+from api.v1.views.payment import *
